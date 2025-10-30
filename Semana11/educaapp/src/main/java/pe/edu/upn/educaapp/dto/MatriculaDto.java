555b937bf9abe636a8e7177d556bb3a26b0e3a5d
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2025-10-30 11:58:49 -0500

    Semana 11

--- Semana11/educaapp/src/main/java/pe/edu/upn/educaapp/dto/MatriculaDto.java
+++ Semana11/educaapp/src/main/java/pe/edu/upn/educaapp/dto/MatriculaDto.java
@@ -9,12 +9,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MatriculaDto {
 	
-	private int idCurso;
-	private int idAlumno;
-	private String tipo;
-	private double precio;
-	private int cuotas;
-	private int idEmpleado;
-	private String estado;
+	private int idCurso; // Codigo del curso.
+	private int idAlumno; // Codigo del estudiante.
+	private String tipo; // Tipo de matricula, puede ser: REGULAR, MEDIABECA o BECA.
+	private int cuotas; // La cantidad de cuotas en que se paga el precio, puede ser 1, 2 o 3.
+	private int idEmpleado; // El codigo del eempleado que registra la matricula.
+	private double precio; // El precio del curso.
+	private String estado; // El estado del proceso.
 	
 }
--- Semana11/educaapp/src/main/java/pe/edu/upn/educaapp/rest/EducaRest.java
+++ Semana11/educaapp/src/main/java/pe/edu/upn/educaapp/rest/EducaRest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import pe.edu.upn.educaapp.dto.MatriculaDto;
 import pe.edu.upn.educaapp.service.ConsultasService;
 import pe.edu.upn.educaapp.service.ProcesosService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/educa/api")
 public class EducaRest {
