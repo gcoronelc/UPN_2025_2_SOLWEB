dfc1555fe3f31dc5d1e349c9d551f57e61418426
ROsangela <rabregum.2012@gmail.com>
2025-11-08 21:11:41 -0500

    Semana 12

--- Semana12/SQLQuery1.sql
+++ Semana12/SQLQuery1.sql
@@ -5,9 +5,13 @@ sp_help empleado
 go
 
 select emp_id id, emp_apellido apellido, emp_nombre nombre,
-emp_email email, emp_usuario usuario, '*****' clave
+emp_direccion direccion, emp_email email, emp_usuario usuario, '*****' clave
 from empleado
 where emp_usuario='ecastillo' and emp_clave='hastalavista';
+go
+
+select * from curso where cur_id=6;
+select * from MATRICULA where cur_id=6;
 go
 
 
